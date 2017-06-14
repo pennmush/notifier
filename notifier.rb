a1162cf9acf0f26e0e7646a93d0e2a22aeede8db
Tim Krajcar <allegro@conmolto.org>
2017-06-14 13:05:26 -0700

    A more safe escaping for MUSH of inputs from the Github API.

--- notifier.rb
+++ notifier.rb
@@ -13,25 +13,29 @@ get '/' do
   "The Notifier is set up and running properly and will connect to #{settings.mush_host} #{settings.mush_port}. See https://github.com/pennmush/notifier for more information."
 end
 
+get '/test_escape' do
+  mush_escape("Hello(), %c! [ansi(h,test)]. https://github.com/onetwothree")
+end
+
 post '/' do
   payload = JSON.parse(params[:payload])
   return unless payload["repository"]["full_name"] == "pennmush/pennmush" # Don't send notifications unless this is the real deal.
 
   github_sender_name = payload["sender"]["login"]
-  sender_name = settings.names.has_key?(github_sender_name) ? settings.names[github_sender_name] : github_sender_name
+  sender_name = mush_escape(settings.names.has_key?(github_sender_name) ? settings.names[github_sender_name] : github_sender_name)
 
   if payload["comment"]
-    message = "[ansi(h,lit(#{sender_name}))] added a comment to issue #[lit(#{payload["issue"]["number"]})] ([lit(#{payload["issue"]["title"]})]. [lit(#{payload["comment"]["html_url"]})]"
+    message = "[ansi(h,#{sender_name})] added a comment to issue ##{mush_escape(payload["issue"]["number"])} (#{mush_escape(payload["issue"]["title"])}). #{mush_escape(payload["comment"]["html_url"])}"
   elsif payload["issue"]
     return unless ["opened", "closed", "reopened"].include? payload["action"]
-    message = "[ansi(h,lit(#{sender_name}))] [lit(#{payload["action"]})] issue #[lit(#{payload["issue"]["number"]})] ([lit(#{payload["issue"]["title"]})]). [lit(#{payload["issue"]["html_url"]})]"
+    message = "[ansi(h,#{sender_name})] #{mush_escape(payload["action"])} issue ##{mush_escape(payload["issue"]["number"])} (#{mush_escape(payload["issue"]["title"])}). #{mush_escape(payload["issue"]["html_url"])}"
   elsif payload["pull_request"]
     return unless ["opened", "closed", "reopened"].include? payload["action"]
     action = payload["pull_request"]["merged"] ? "merged" : payload["action"] # Merged PRs still have a 'closed' action, so need to look for merged key.
-    message = "[ansi(h,lit(#{sender_name}))] [lit(#{action})] PR #[lit(#{payload["pull_request"]["number"]})] ([lit(#{payload["pull_request"]["title"]})]). [lit(#{payload["pull_request"]["html_url"]})]"
+    message = "[ansi(h,#{sender_name})] #{mush_escape(action)} PR ##{mush_escape(payload["pull_request"]["number"])} (#{mush_escape(payload["pull_request"]["title"])}). #{mush_escape(payload["pull_request"]["html_url"])}"
   elsif payload["commits"]
     return unless payload["ref"] == "refs/heads/master" # We only want to notify on pushes to master, not to branches.
-    message = "[ansi(h,lit(#{sender_name}))] pushed [lit(#{payload["commits"].count})] commit#{payload["commits"].count == 1 ? '' : 's'} to master. [lit(#{payload["compare"]})]"
+    message = "[ansi(h,#{sender_name})] pushed #{payload["commits"].count} commit#{payload["commits"].count == 1 ? '' : 's'} to master. #{mush_escape(payload["compare"])})]"
   else
     return 401 # We got an event we shouldn't have.
   end
@@ -43,4 +47,8 @@ post '/' do
   tn.close
 
   return message
+end
+
+def mush_escape(str)
+  str.gsub(/([\%\;\[\]\{\}\\\(\)\,\^\$])/, '%\1')
 end
